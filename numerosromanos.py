33de6f73daf61fd4fa0a95f076f2e5c9ada0a701
Carla Sofia Centeleghe <81233915+Carla-Sofia-Centeleghe@users.noreply.github.com>
2022-09-28 09:41:54 -0300

    carga codigo

---
+++ numerosromanos.py
@@ -0,0 +1,30 @@
+import unittest
+# Numero decimal a Romano
+# Llega hasta el 3999
+# Ni idea como se escribe el 4000 en romano
+#Carla S. Centeleghe
+
+
+def Roman(num):
+
+    m = ["", "M", "MM", "MMM"]
+    c = ["", "C", "CC", "CCC", "CD", "D",
+         "DC", "DCC", "DCCC", "CM"]
+    x = ["", "X", "XX", "XXX", "XL", "L",
+         "LX", "LXX", "LXXX", "XC"]
+    i = ["", "I", "II", "III", "IV", "V",
+         "VI", "VII", "VIII", "IX"]
+
+    miles = m[num // 1000]
+    cientos = c[(num % 1000) // 100]
+    decenas = x[(num % 100) // 10]
+    unos = i[num % 10]
+
+    result = (miles + cientos + decenas + unos)
+
+    return result
+
+
+if __name__ == "__main__":
+    number = int(input('Ingresa un numero: '))
+    print(Roman(number))
---
+++ test.py
@@ -0,0 +1,61 @@
+import unittest
+import numeroromanos
+
+
+class TestRomanToNumber(unittest.TestCase):
+
+    def test_1(self):
+        roman = numeroromanos.Roman(1)
+        self.assertEqual(roman, "I")
+
+    def test_4(self):
+        roman = numeroromanos.Roman(4)
+        self.assertEqual(roman, "IV")
+
+    def test_5(self):
+        roman = numeroromanos.Roman(5)
+        self.assertEqual(roman, "V")
+
+    def test_9(self):
+        roman = numeroromanos.Roman(9)
+        self.assertEqual(roman, "IX")
+
+    def test_10(self):
+        roman = numeroromanos.Roman(10)
+        self.assertEqual(roman, "X")
+
+    def test_40(self):
+        roman = numeroromanos.Roman(40)
+        self.assertEqual(roman, "XL")
+
+    def test_50(self):
+        roman = numeroromanos.Roman(50)
+        self.assertEqual(roman, "L")
+
+    def test_100(self):
+        roman = numeroromanos.Roman(100)
+        self.assertEqual(roman, "C")
+
+    def test_500(self):
+        roman = numeroromanos.Roman(500)
+        self.assertEqual(roman, "D")
+
+    def test_900(self):
+        roman = numeroromanos.Roman(900)
+        self.assertEqual(roman, "CM")
+
+    def test_1000(self):
+        roman = numeroromanos.Roman(1000)
+        self.assertEqual(roman, "M")
+
+    def test_1549(self):
+        roman = numeroromanos.Roman(1549)
+        self.assertEqual(roman, "MDXLIX")
+
+    def test_3999(self):
+        roman = numeroromanos.Roman(3999)
+        self.assertEqual(roman, "MMMCMXCIX")
+
+
+if __name__ == '__main__':
+    unittest.main()
